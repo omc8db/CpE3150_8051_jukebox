ac5208c6685cc2e79a48fe3449f477ff2336cec3
Owen Chiaventone <omc8db@mst.edu>
2015-04-28 12:57:33 -0500

    Create project with Simon Board headers

---
+++ simon2.h
@@ -0,0 +1,33 @@
+/*--------------------------------------------------------------------------
+simon2.h
+
+Header file for the Simon Board
+
+--------------------------------------------------------------------------*/
+
+#ifndef SIMON_2
+#define SIMON_2
+
+#define BUZZER p1.7
+
+#define LED1_RED p2.4
+#define LED2_AMB p0.6
+#define LED3_YEL p2.5
+#define LED4_YEL p0.5
+#define LED5_RED p1.6
+#define LED6_GRN p0.7
+#define LED7_GRN p2.7
+#define LED8_RED p0.4
+#define LED9_AMB p2.6
+
+#define SW1 p2.0
+#define SW2 p0.2
+#define SW3 p2.1
+#define SW4 p0.1
+#define SW5 p1.4
+#define SW6 p0.3
+#define SW7 p2.3
+#define SW8 p0.0
+#define SW9 p2.2
+
+#endif //SIMON_2
